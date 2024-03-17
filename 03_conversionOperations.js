06a92da32e4af5d5c2eb416b04b0f4dffe66f073
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-03-17 22:28:50 +0530

    prefix and postfix incrementing

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -1,7 +1,7 @@
-let score = "chethan"
-console.log(typeof (score));
+// let score = "chethan"
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 // console.log(typeof (valueInNumber));
 // console.log(valueInNumber);
 
@@ -11,16 +11,51 @@ let valueInNumber = Number(score);
 // true => 1
 // false => 0
 
-let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true
 //0 => false
 // "" => false
 //"hitesh" => true
 
-let someNumber = 33
-let strNumber = String(someNumber);
-console.log(strNumber, typeof strNumber);
+// let someNumber = 33
+// let strNumber = String(someNumber);
+// console.log(strNumber, typeof strNumber);
+
+//*************************** Operations *****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(2 + '1');
+console.log('1' + 2 + '2');
+console.log(1 + 2 + '2');
+
+//mdn examples
+//postfix => returns value before incrementing
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+//prefix => returns value after incrementing
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
 
