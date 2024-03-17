049162fb7d847ccd93c8ce059d53750bc2d76df4
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-03-17 21:46:48 +0530

    data-types

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3);  //we are using node.js not browser
+
+let name = "chethan"
+let age = 18
+let isLoggedIn = true;
+
+//Data Types
+//number => 2 to power 53
+//bigInt 
+//string
+//boolean => true / false
+//null => standalone value
+//undefined => 
+//symbol => unique
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
+
+
+
